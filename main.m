e4153df26bee965596bc67b0feed80556527ede2
He Chen <chenhe95@hotmail.com>
2016-11-27 18:34:11 -0500

    82% correct (on training set)

--- main.m
+++ main.m
@@ -7,19 +7,19 @@ load('train_set/train_img_prob.mat');
 labels = Y;
 word_counts_processed = X;
 
-wc = fitcknn(word_counts_processed, labels, 'NumNeighbors', 10);
+wc = fitcknn(word_counts_processed, labels, 'NumNeighbors', 20);
 wc_out = predict(wc, word_counts_processed);
 wc_acc = 1 / sum(labels ~= wc_out);
 
-cnn = fitcknn(train_cnn_feat, labels, 'NumNeighbors', 10);
+cnn = fitcknn(train_cnn_feat, labels, 'NumNeighbors', 5);
 cnn_out = predict(cnn, train_cnn_feat);
 cnn_acc = 1 / sum(labels ~= cnn_out);
 
-color = fitcknn(train_color, labels,  'NumNeighbors', 10);
+color = fitcknn(train_color, labels,  'NumNeighbors', 5);
 color_out = predict(color, train_color);
 color_acc = 1 / sum(labels ~= color_out);
 
-prob = fitcknn(train_img_prob, labels, 'NumNeighbors', 10);
+prob = fitcknn(train_img_prob, labels, 'NumNeighbors', 5);
 prob_out = predict(prob, train_img_prob);
 prob_acc = 1 / sum(labels ~= prob_out);
 
