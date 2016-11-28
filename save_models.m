9bd5faa42ef167003f0fd998fb35f628a109ac90
He Chen <chenhe95@hotmail.com>
2016-11-28 03:49:22 -0500

    save models fn

---
+++ save_models.m
@@ -0,0 +1,22 @@
+function save_models(file_name, models)
+
+BASE_DIR = fullfile('models');
+BACKUP_DIR = fullfile('models', 'backup');
+
+file_name = [file_name '.mat']
+
+file_path = fullfile(BASE_DIR, file_name);
+
+if exist(file_path, file_name)
+    index = 0;
+    backup_path = fullfile(BACKUP_DIR, [file_name num2str(index)]);
+    while exist(backup_path, 'file')
+        index = index + 1;
+        backup_path = fullfile(BACKUP_DIR, [file_name num2str(index)]);
+    end
+    movefile(file_path, backup_path);
+end
+
+save(file_path, 'models');
+
+end
