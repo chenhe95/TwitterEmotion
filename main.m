1806c9cc38c1b394d333a5deb29d2fa41e496fe2
He Chen <chenhe95@hotmail.com>
2016-11-27 20:01:17 -0500

    98% train accuracy

--- main.m
+++ main.m
@@ -5,22 +5,22 @@ load('train_set/train_img_prob.mat');
 
 % will do preprocessing later
 labels = Y;
-word_counts_processed = X;
+word_counts_processed = full(X);
 
-wc = fitcknn(word_counts_processed, labels, 'NumNeighbors', 20);
-wc_out = predict(wc, word_counts_processed);
+wc_model = fitcsvm(word_counts_processed, labels);
+wc_out = predict(wc_model, word_counts_processed);
 wc_acc = 1 / sum(labels ~= wc_out);
 
-cnn = fitcknn(train_cnn_feat, labels, 'NumNeighbors', 5);
-cnn_out = predict(cnn, train_cnn_feat);
+cnn_model = fitcknn(train_cnn_feat, labels, 'NumNeighbors', 5);
+cnn_out = predict(cnn_model, train_cnn_feat);
 cnn_acc = 1 / sum(labels ~= cnn_out);
 
-color = fitcknn(train_color, labels,  'NumNeighbors', 5);
-color_out = predict(color, train_color);
+color_model = fitcknn(train_color, labels,  'NumNeighbors', 5);
+color_out = predict(color_model, train_color);
 color_acc = 1 / sum(labels ~= color_out);
 
-prob = fitcknn(train_img_prob, labels, 'NumNeighbors', 5);
-prob_out = predict(prob, train_img_prob);
+prob_model = fitcknn(train_img_prob, labels, 'NumNeighbors', 5);
+prob_out = predict(prob_model, train_img_prob);
 prob_acc = 1 / sum(labels ~= prob_out);
 
 total_acc = wc_acc + cnn_acc + color_acc + prob_acc;
