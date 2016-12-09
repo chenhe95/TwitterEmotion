6d53a72255d3cd95d2fc3f3f3df3f644072a98bc
Henri Maxime Demoulin <maxdml@seas.upenn.edu>
2016-12-09 16:32:55 -0500

    train and predict random forest

---
+++ predict_labels_random_forest.m
@@ -0,0 +1,27 @@
+function [Y_hat] = predict_labels(word_counts, cnn_feat, prob_feat, color_feat, raw_imgs, raw_tweets)
+% Inputs:   word_counts     nx10000 word counts features
+%           cnn_feat        nx4096 Penultimate layer of Convolutional
+%                               Neural Network features
+%           prob_feat       nx1365 Probabilities on 1000 objects and 365
+%                               scene categories
+%           color_feat      nx33 Color spectra of the images (33 dim)
+%           raw_imgs        nx30000 raw images pixels
+%           raw_tweets      nx1 cells containing all the raw tweets in text
+% Outputs:  Y_hat           nx1 predicted labels (1 for joy, 0 for sad)
+
+%% Load models
+load ./models.mat
+
+%% preprocess
+word_counts_processed = full(double(word_counts ~= 0));
+
+word_counts_processed(:, preprocess) = [];
+word_counts_processed(:, c_removed) = [];
+
+% wc_score_test = word_counts_processed * saved_pc;
+
+%% Predict
+wc_out = predict(wc_model, word_counts_processed);
+Y_hat = str2num(cell2mat(wc_out));
+
+end
